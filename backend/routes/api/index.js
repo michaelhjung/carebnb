c0e072cc735843dd569e6e0f696e1ad212993e82
Michael Jung <jung.michaelh@gmail.com>
2022-08-26 11:28:14 -0700

    Re-add commented-out test user auth middlewares for future use

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
+
 /*--------------------------------- ROUTERS ---------------------------------*/
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
@@ -17,6 +18,37 @@ router.use('/users', usersRouter);
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
 });
+
+// // GET /api/set-token-cookie
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// });
+
+// // GET /api/restore-user
+// router.get(
+//     '/restore-user',
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+// // GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
 
 /*---------------------------------------------------------------------------*/
 
