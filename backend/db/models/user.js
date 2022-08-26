94ae498452697c7d9d71272ecf3987bda280f922
Michael Jung <jung.michaelh@gmail.com>
2022-08-25 17:11:35 -0700

    Add firstName and lastName to migrations and models

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -51,6 +51,22 @@ module.exports = (sequelize, DataTypes) => {
 
   User.init(
     {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isAlpha: true,
+          len: [2, 30]
+        }
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isAlpha: true,
+          len: [2, 30]
+        }
+      },
       username: {
         type: DataTypes.STRING,
         allowNull: false,
