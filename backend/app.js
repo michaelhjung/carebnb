139a1d8ad3515441692eb8dd99b9c425c6afd44c
Michael Jung <jung.michaelh@gmail.com>
2022-08-29 14:36:46 -0700

    Sign up user & error handling, log in user, get current user

--- backend/app.js
+++ backend/app.js
@@ -86,6 +86,7 @@ app.use((err, _req, res, _next) => {
     res.json({
         title: err.title || 'Server Error',
         message: err.message,
+        statusCode: (err.status || 500),
         errors: err.errors,
         stack: isProduction ? null : err.stack
     });
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,6 +4,7 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const { application } = require('express');
 
 const router = express.Router();
 
@@ -11,18 +12,22 @@ const router = express.Router();
 const validateSignup = [
     check('email')
         .exists({ checkFalsy: true })
+        .withMessage('Invalid email')
         .isEmail()
         .withMessage('Please provide a valid email.'),
     check('firstName')
         .exists({ checkFalsy: true })
+        .withMessage('First Name is required')
         .isLength({ min: 2 })
         .withMessage('Please provide a first name with at least 2 characters.'),
     check('lastName')
         .exists({ checkFalsy: true })
+        .withMessage('Last Name is required')
         .isLength({ min: 2 })
         .withMessage('Please provide a last name with at least 2 characters.'),
     check('username')
         .exists({ checkFalsy: true })
+        .withMessage('Username is required')
         .isLength({ min: 4 })
         .withMessage('Please provide a username with at least 4 characters.'),
     check('username')
@@ -31,6 +36,7 @@ const validateSignup = [
         .withMessage('Username cannot be an email.'),
     check('password')
         .exists({ checkFalsy: true })
+        .withMessage('Password is required')
         .isLength({ min: 6 })
         .withMessage('Password must be 6 characters or more.'),
     handleValidationErrors
@@ -42,15 +48,33 @@ const validateSignup = [
 router.post(
     '/',
     validateSignup,
-    async (req, res) => {
+    async (req, res, next) => {
         const { firstName, lastName, email, password, username } = req.body;
-        const user = await User.signup({ firstName, lastName, email, username, password });
 
-        await setTokenCookie(res, user);
+        const existingEmail = await User.findOne({ where: { email } });
+        const existingUsername = await User.findOne({ where: { username } });
 
-        return res.json({
-            user
-        });
+        if (existingEmail) {
+            const err = new Error("User already exists");
+            err.status = 403;
+            err.errors = { "email": "User with that email already exists"}
+            next(err);
+        }
+        else if (existingUsername) {
+            const err = new Error("User already exists");
+            err.status = 403;
+            err.errors = { "username": "User with that username already exists"}
+            next(err);
+        }
+        else {
+            const user = await User.signup({ firstName, lastName, email, username, password });
+
+            await setTokenCookie(res, user);
+
+            return res.json({
+                user
+            });
+        }
     }
 );
 
