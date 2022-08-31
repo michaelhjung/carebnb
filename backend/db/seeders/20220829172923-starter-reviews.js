537c0e9204d813874ec4dee483bb388b6a6f8a84
Michael Jung <jung.michaelh@gmail.com>
2022-08-31 13:36:56 -0700

    Add more seed reviews

--- backend/db/seeders/20220829172923-starter-reviews.js
+++ backend/db/seeders/20220829172923-starter-reviews.js
@@ -34,6 +34,30 @@ module.exports = {
             review: "I got lost in a blizzard! STAY AWAY!!!",
             stars: 1
         },
+        {
+            spotId: 4,
+            userId: 4,
+            review: "Some ugly looking creatures tried to steal the engagement ring I bought for my girlfriend! Don't come here!",
+            stars: 1
+        },
+        {
+            spotId: 6,
+            userId: 4,
+            review: "I came here to heal my pokemon and rest up, but it's not actually a pokemon center! At least the weather was nice...",
+            stars: 2
+        },
+        {
+            spotId: 7,
+            userId: 9,
+            review: "This place was so awesome! I LOVED IT.",
+            stars: 5
+        },
+        {
+            spotId: 7,
+            userId: 5,
+            review: "Pretty nice place. Might have to buy it. :)",
+            stars: 5
+        },
     ]);
   },
 
