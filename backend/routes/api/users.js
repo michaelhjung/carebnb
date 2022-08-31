82209d43098634766de57be3140260dbc9280463
Michael Jung <jung.michaelh@gmail.com>
2022-08-31 10:06:14 -0700

    Complete all spots routes - except body validations formatting (no kvp)

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,7 +4,6 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
-const { application } = require('express');
 
 const router = express.Router();
 
