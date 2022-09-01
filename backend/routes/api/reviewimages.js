f254220059aacd6b037443a2a481e87e15b0c83e
Michael Jung <jung.michaelh@gmail.com>
2022-09-01 14:06:16 -0700

    Complete routes for delete reviewImage and delete spotImage, local test

--- backend/routes/api/reviewimages.js
+++ backend/routes/api/reviewimages.js
@@ -7,12 +7,32 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-/*------------------------------- MIDDLEWARE -------------------------------*/
-
-/*--------------------------------------------------------------------------*/
-
 /*--------------------------------- ROUTES ---------------------------------*/
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+    const reviewImage = await ReviewImage.findByPk(req.params.imageId);
+    if (reviewImage) {
+        // check if review belongs to user
+        const review = await Review.findByPk(reviewImage.reviewId);
+        if (req.user.id === review.userId) {
+            await reviewImage.destroy();
 
+            res.json({
+                message: "Successfully deleted",
+                statusCode: 200
+            });
+        }
+
+        else res.status(403).json({
+            message: "Forbidden",
+            statusCode: 403
+        });
+    }
+
+    else res.status(404).json({
+        message: "Review Image couldn't be found",
+        statusCode: 404
+    });
+});
 
 
 /*--------------------------------------------------------------------------*/
--- backend/routes/api/spotimages.js
+++ backend/routes/api/spotimages.js
@@ -7,12 +7,32 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-/*------------------------------- MIDDLEWARE -------------------------------*/
-
-/*--------------------------------------------------------------------------*/
-
 /*--------------------------------- ROUTES ---------------------------------*/
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+    const spotImage = await SpotImage.findByPk(req.params.imageId);
+    if (spotImage) {
+        // check if spot belongs to user
+        const spot = await Spot.findByPk(spotImage.spotId);
+        if (req.user.id === spot.ownerId) {
+            await spotImage.destroy();
 
+            res.json({
+                message: "Successfully deleted",
+                statusCode: 200
+            });
+        }
+
+        else res.status(403).json({
+            message: "Forbidden",
+            statusCode: 403
+        });
+    }
+
+    else res.status(404).json({
+        message: "Spot Image couldn't be found",
+        statusCode: 404
+    });
+});
 
 
 /*--------------------------------------------------------------------------*/
