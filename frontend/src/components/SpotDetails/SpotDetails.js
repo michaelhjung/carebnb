c454e8edd42d859796f2108e6c33927440a9d98f
Michael Jung <jung.michaelh@gmail.com>
2022-09-19 13:52:51 -0700

    Add spot details component -- tofix: img not rendering

--- frontend/src/components/SpotDetails/SpotDetails.js
+++ frontend/src/components/SpotDetails/SpotDetails.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import * as spotsActions from '../../store/spots';
 
 export default function SpotDetails({ spots }) {
+    const dispatch = useDispatch();
     const { spotId } = useParams();
-    const spot = spots[spotId];
+    const spot = useSelector(state => state.spots.singleSpot);
+    useEffect(() => {
+        dispatch(spotsActions.getSingleSpot(spotId));
+    }, [dispatch, spotId]);
 
     if (!spot) return null;
 
     return (
-        <div>
-            SpotDetails for spot id: {spotId}
+        <div className='spot-details--container'>
+            <h1 className='spot-details--title-main'>{spot.name}</h1>
+
+            <div className='spot-details--sub-title-reviews'>
+                <span className='spot-details--sub-title-rating'>
+                    <i className="fa-solid fa-star icon--star" />
+                    {spot.avgStarRating}
+                </span>
+                <span className='spot-details--sub-title-dot'> · </span>
+                <span className='spot-details--sub-title-numReviews'>{spot.numReviews} reviews</span>
+                <span className='spot-details--sub-title-dot'> · </span>
+                <span className='spot-details--sub-title-owner'>{spot.city}, {spot.state}, {spot.country}</span>
+            </div>
+
+            <div className='spot-details--imgs-container'>
+                {spot.spotImages.forEach(img => {
+                    console.log("img:", img)
+                    return <img src={img.url} alt={img.url} className='spot-details--img' />
+                })}
+            </div>
+
+            <div className='spot-details--title-host-info'>
+                <h2>Hosted by {spot.Owner.firstName} {spot.Owner.lastName}</h2>
+            </div>
+
+            <div className='spot-details--description-container'>
+                <p className='spot-details--description'>{spot.description}</p>
+            </div>
         </div>
     )
 }
