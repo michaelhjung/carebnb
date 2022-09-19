ad6f9ba758eb1c07ede656dd6a3ea992679fed47
Michael Jung <jung.michaelh@gmail.com>
2022-09-19 10:43:26 -0700

    Fix get spots bug

--- frontend/src/components/SpotsBrowser/index.js
+++ frontend/src/components/SpotsBrowser/index.js
@@ -1,22 +1,23 @@
 import React, { useEffect } from 'react';
-import { NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import * as spotsActions from '../../store/spots';
 
 export default function SpotsBrowser() {
     const dispatch = useDispatch();
+    const spots = useSelector(state => state.spots.allSpots);
+
     useEffect(() => {
         dispatch(spotsActions.getSpots());
     }, [dispatch]);
-    const spots = useSelector(state => state.spots);
-    const spotsArr = Object.values(spots);
-    console.log("FINAL SPOTS RETURNED AFTER FETCH:", spots);
-    console.log("SPOTS ARRAY:", spotsArr);
-    console.log("SPOTS ARRAY INDEX 0:", spotsArr[0]);
 
+    console.log("FINAL SPOTS RETURNED AFTER FETCH:", spots);
+
+    if (!spots) return null;
     return (
         <main className='container--spots'>
-
+            {Object.values(spots).map(spot => (
+                <div key={spot.id}>{spot.name}</div>
+            ))}
         </main>
     )
 }
