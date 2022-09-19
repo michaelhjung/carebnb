d5c443d99cef4b957ddaf2f1c048a9ce505b3e48
Michael Jung <jung.michaelh@gmail.com>
2022-09-19 13:02:21 -0700

    Refactor spots

--- frontend/src/components/SpotsBrowser/index.js
+++ frontend/src/components/SpotsBrowser/index.js
@@ -1,28 +1,22 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import * as spotsActions from '../../store/spots';
 import './SpotsBrowser.css'
 import SpotCard from '../SpotDetails/SpotCard';
 
-export default function SpotsBrowser() {
-    const dispatch = useDispatch();
-    const spots = useSelector(state => state.spots.allSpots);
-
-    useEffect(() => {
-        dispatch(spotsActions.getSpots());
-    }, [dispatch]);
-
+export default function SpotsBrowser({ spots }) {
     if (!spots) return null;
+
     return (
-        <main className='container--spots'>
-            {Object.values(spots).map(spot => (
-                <div key={spot.id} className="spot-card">
-                    <NavLink to={`/api/spots/${spot.id}`} className="link--spot">
-                        <SpotCard spot={spot} />
-                    </NavLink>
-                </div>
-            ))}
-        </main>
+        <>
+            <main className='container--spots'>
+                {Object.values(spots).map(spot => (
+                    <div key={spot.id} className="spot-card">
+                        <NavLink to={`/api/spots/${spot.id}`} className="link--spot">
+                            <SpotCard spot={spot} />
+                        </NavLink>
+                    </div>
+                ))}
+            </main>
+        </>
     )
 }
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -1,12 +1,18 @@
 // import { csrfFetch } from './csrf';
 /* ----------------------------- ACTION TYPES: ----------------------------- */
-const LOAD_ALL = '/spots/LOAD_ALL'
+const LOAD_ALL = '/spots/LOAD_ALL';
+const LOAD_ONE = '/spots/LOAD_ONE';
 
 
 /* ---------------------------- ACTION CREATORS: ---------------------------- */
 const loadAll = (spots) => ({
     type: LOAD_ALL,
     payload: spots
+});
+
+const loadOne = (spot) => ({
+    type: LOAD_ONE,
+    payload: spot
 });
 
 
@@ -16,8 +22,18 @@ export const getSpots = () => async dispatch => {
 
     if (response.ok) {
         const spots = await response.json();
-        console.log("JSONIFIED SPOTS DATA AFTER THUNK:", spots);
+        // console.log("JSONIFIED SPOTS DATA AFTER THUNK:", spots);
         dispatch(loadAll(spots));
+    }
+}
+
+export const getSingleSpot = (spotId) => async dispatch => {
+    const response = await fetch(`/api/spots/${spotId}`);
+
+    if (response.ok) {
+        const spot = await response.json();
+        console.log("JSONIFIED SINGLE SPOT DATA AFTER THUNK:", spot);
+        dispatch(loadOne(spot));
     }
 }
 
@@ -30,11 +46,18 @@ const spotsReducer = (state = initialState, action) => {
     switch (action.type) {
         case LOAD_ALL:
             newState = { ...state }
-            console.log("LOAD_ALL ACTION.PAYLOAD IS:", action.payload);
+            // console.log("LOAD_ALL ACTION.PAYLOAD IS:", action.payload);
             const newAllSpots = {};
             action.payload.Spots.forEach(spot => newAllSpots[spot.id] = spot);
             newState.allSpots = newAllSpots;
-            console.log("NEWSTATE AFTER LOAD_ALL ACTION:", newState);
+            // console.log("NEWSTATE AFTER LOAD_ALL ACTION:", newState);
+            return newState;
+        case LOAD_ONE:
+            newState = { ...state }
+            console.log("LOAD_ONE ACTION.PAYLOAD IS:", action.payload);
+            const newSingleSpot = {};
+            newState.singleSpot = newSingleSpot;
+            console.log("NEWSTATE AFTER LOAD_ONE ACTION:", newState);
             return newState;
         default:
             return state;
