2f825b2457c68d9f39b9cb35de98b39818f6d5a0
Michael Jung <jung.michaelh@gmail.com>
2022-09-22 20:05:25 -0700

    Fix border line issue on signup login forms

--- frontend/src/components/LoginSignupForm/LoginForm.js
+++ frontend/src/components/LoginSignupForm/LoginForm.js
@@ -28,56 +28,58 @@ function LoginForm({ setShowMenu, closeMenu }) {
 
     return (
         <div className='container--login-signup-forms' id='container--login-form'>
-            <div className='title-login-signup' id='title-login'>
+            <div className='title-login-signup container--login-signup-top' id='title-login'>
                 Log in
             </div>
 
             <div className="border-div"></div>
 
-            <h1 className='welcome-login-signup welcome-login'>Welcome to Carebnb</h1>
+            <div className="container--login-signup-bottom">
+                <h1 className='welcome-login-signup welcome-login'>Welcome to Carebnb</h1>
 
-            <ul className="list--errors">
-                {validationErrors.map((error, idx) => (
-                    <li key={idx} className='error-li'>{error}</li>
-                ))}
-            </ul>
+                <ul className="list--errors">
+                    {validationErrors.map((error, idx) => (
+                        <li key={idx} className='error-li'>{error}</li>
+                    ))}
+                </ul>
 
-            <form onSubmit={handleSubmit} className="form--login-signup form--login">
+                <form onSubmit={handleSubmit} className="form--login-signup form--login">
 
-                <div className="container--login-signup-fields container--login-fields">
-                    <div id="container--login-field-credential" className="container--login-signup-field container--login-field" >
-                        <input
-                            type="text"
-                            value={credential}
-                            onChange={(e) => setCredential(e.target.value)}
-                            placeholder="Username or Email"
-                            required
-                            className="login-signup-field login-field"
-                            id="login-field--credential"
-                        />
+                    <div className="container--login-signup-fields container--login-fields">
+                        <div id="container--login-field-credential" className="container--login-signup-field container--login-field" >
+                            <input
+                                type="text"
+                                value={credential}
+                                onChange={(e) => setCredential(e.target.value)}
+                                placeholder="Username or Email"
+                                required
+                                className="login-signup-field login-field"
+                                id="login-field--credential"
+                            />
+                        </div>
+                        <div id="container--login-field-password" className="container--login-signup-field container--login-field">
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder="Password"
+                                required
+                                className="login-signup-field login-field"
+                                id="login-field--password"
+                            />
+                        </div>
                     </div>
-                    <div id="container--login-field-password" className="container--login-signup-field container--login-field">
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            placeholder="Password"
-                            required
-                            className="login-signup-field login-field"
-                            id="login-field--password"
-                        />
-                    </div>
-                </div>
 
-                <button
-                    type="submit"
-                    id='login-submit-button'
-                    className='login-signup-submit-button'
-                    disabled={validationErrors.length}
-                >
-                    Log In
-                </button>
-            </form>
+                    <button
+                        type="submit"
+                        id='login-submit-button'
+                        className='login-signup-submit-button'
+                        disabled={validationErrors.length}
+                    >
+                        Log In
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
--- frontend/src/components/LoginSignupForm/SignupForm.js
+++ frontend/src/components/LoginSignupForm/SignupForm.js
@@ -16,11 +16,11 @@ function SignupForm({ setShowMenu, closeMenu }) {
     useEffect(() => {
         const errors = [];
 
-        if (email.length && (!email.includes("@") || !email.includes("."))) errors.push("Inavlid email");
+        if (email.length && (!email.includes("@") || !email.includes("."))) errors.push("Invalid email");
         if (password && confirmPassword && password !== confirmPassword) errors.push("Confirm Password field must be the same as the Password field");
 
         setValidationErrors(errors);
-    }, [email, password]);
+    }, [email, password, confirmPassword]);
 
     if (sessionUser) return <Redirect to="/" />;
 
@@ -49,97 +49,100 @@ function SignupForm({ setShowMenu, closeMenu }) {
 
     return (
         <div className="container--login-signup-forms" id="container--signup-form">
-            <div className='title-login-signup title-signup'>
+            <div className='title-login-signup title-signup container--login-signup-top'>
                 Sign Up
             </div>
 
             <div className="border-div"></div>
 
-            <h1 className='welcome-login-signup welcome-signup'>Welcome to Carebnb</h1>
+            <div className="container--login-signup-bottom">
 
-            <form onSubmit={handleSubmit} className="form--login-signup form--signup">
-                <ul className="list--errors">
-                    {validationErrors.map((error, idx) => <li key={idx} className="error-li">{error}</li>)}
-                </ul>
+                <h1 className='welcome-login-signup welcome-signup'>Welcome to Carebnb</h1>
 
-                <div className="container--login-signup-fields container--signup-fields">
-                    <div id="container--signup-field-first-name" className="container--login-signup-field container--signup-field" >
-                        <input
-                            type="text"
-                            value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)}
-                            required
-                            placeholder="First Name"
-                            className="login-signup-field signup-field"
-                            id="signup-field--first-name"
-                        />
-                    </div>
-                    <div id="container--signup-field-last-name" className="container--login-signup-field container--signup-field" >
-                        <input
-                            type="text"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                            required
-                            placeholder="Last Name"
-                            className="login-signup-field signup-field"
-                            id="signup-field--last-name"
-                        />
-                    </div>
-                    <div id="container--signup-field-email" className="container--login-signup-field container--signup-field" >
-                        <input
-                            type="text"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                            placeholder="Email"
-                            className="login-signup-field signup-field"
-                            id="signup-field--email"
-                        />
-                    </div>
-                    <div id="container--signup-field-username" className="container--login-signup-field container--signup-field" >
-                        <input
-                            type="text"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            required
-                            placeholder="Username"
-                            className="login-signup-field signup-field"
-                            id="signup-field--username"
-                        />
-                    </div>
-                    <div id="container--signup-field-password" className="container--login-signup-field container--signup-field" >
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                            placeholder="Password"
-                            className="login-signup-field signup-field"
-                            id="signup-field--password"
-                        />
-                    </div>
-                    <div id="container--signup-field-confirm-password" className="container--login-signup-field container--signup-field" >
-                        <input
-                            type="password"
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
-                            required
-                            placeholder="Confirm Password"
-                            className="login-signup-field signup-field"
-                            id="signup-field--confirm-password"
-                        />
-                    </div>
-                </div>
+                <form onSubmit={handleSubmit} className="form--login-signup form--signup">
+                    <ul className="list--errors">
+                        {validationErrors.map((error, idx) => <li key={idx} className="error-li">{error}</li>)}
+                    </ul>
 
-                <button
-                    type="submit"
-                    id='signup-submit-button'
-                    className='login-signup-submit-button'
-                    disabled={validationErrors.length}
-                >
-                    Sign Up
-                </button>
-            </form>
+                    <div className="container--login-signup-fields container--signup-fields">
+                        <div id="container--signup-field-first-name" className="container--login-signup-field container--signup-field" >
+                            <input
+                                type="text"
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                                required
+                                placeholder="First Name"
+                                className="login-signup-field signup-field"
+                                id="signup-field--first-name"
+                            />
+                        </div>
+                        <div id="container--signup-field-last-name" className="container--login-signup-field container--signup-field" >
+                            <input
+                                type="text"
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                                required
+                                placeholder="Last Name"
+                                className="login-signup-field signup-field"
+                                id="signup-field--last-name"
+                            />
+                        </div>
+                        <div id="container--signup-field-email" className="container--login-signup-field container--signup-field" >
+                            <input
+                                type="text"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                                placeholder="Email"
+                                className="login-signup-field signup-field"
+                                id="signup-field--email"
+                            />
+                        </div>
+                        <div id="container--signup-field-username" className="container--login-signup-field container--signup-field" >
+                            <input
+                                type="text"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                                placeholder="Username"
+                                className="login-signup-field signup-field"
+                                id="signup-field--username"
+                            />
+                        </div>
+                        <div id="container--signup-field-password" className="container--login-signup-field container--signup-field" >
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                                placeholder="Password"
+                                className="login-signup-field signup-field"
+                                id="signup-field--password"
+                            />
+                        </div>
+                        <div id="container--signup-field-confirm-password" className="container--login-signup-field container--signup-field" >
+                            <input
+                                type="password"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                required
+                                placeholder="Confirm Password"
+                                className="login-signup-field signup-field"
+                                id="signup-field--confirm-password"
+                            />
+                        </div>
+                    </div>
+
+                    <button
+                        type="submit"
+                        id='signup-submit-button'
+                        className='login-signup-submit-button'
+                        disabled={validationErrors.length}
+                    >
+                        Sign Up
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
